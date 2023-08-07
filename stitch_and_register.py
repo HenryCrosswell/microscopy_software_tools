618083c87e86f56f25f7cbd9aedca5f050189e3c
Henry C <henry.junkmail100@gmail.com>
2023-08-07 18:14:03 +0100

    updated registratino function to use simpleitk

--- stitch_and_register.py
+++ stitch_and_register.py
@@ -1,6 +1,7 @@
 import logging
 import cv2
 import numpy as np
+import SimpleITK as sitk
 
 def register_images(images):
     registered_images = [images[0]]  # The first image is the reference
@@ -10,43 +11,35 @@ def register_images(images):
         image = images[i]
         
         # Perform image registration using feature matching, homography, etc.
-        registered_image = image_registration(registered_images[i - 1], image)
+        registered_image = image_registration_sitk(registered_images[i - 1], image)
         
         registered_images.append(registered_image)
 
     return registered_images
 
-def image_registration(reference, image_to_register):
-    # Initialize ORB detector
-    orb = cv2.ORB_create()
-
-    # Find keypoints and descriptors in the images
-    keypoints_ref, descriptors_ref = orb.detectAndCompute(reference, None)
-    keypoints_to_register, descriptors_to_register = orb.detectAndCompute(image_to_register, None)
-
-    # Create a brute-force matcher object
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-    # Match descriptors
-    matches = bf.match(descriptors_ref, descriptors_to_register)
-
-    # Sort matches by distance
-    matches = sorted(matches, key=lambda x: x.distance)
-
-    # Extract matched keypoints
-    matched_keypoints_ref = [keypoints_ref[match.queryIdx] for match in matches]
-    matched_keypoints_to_register = [keypoints_to_register[match.trainIdx] for match in matches]
-
-    # Calculate homography
-    src_pts = np.float32([kp.pt for kp in matched_keypoints_ref]).reshape(-1, 1, 2)
-    dst_pts = np.float32([kp.pt for kp in matched_keypoints_to_register]).reshape(-1, 1, 2)
+def image_registration_sitk(reference, image_to_register):
+    # Convert images to SimpleITK format
+    reference_sitk = sitk.GetImageFromArray(reference)
+    image_to_register_sitk = sitk.GetImageFromArray(image_to_register)
     
-    homography_matrix, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
-    # Apply homography to transform the image
-    registered_image = cv2.warpPerspective(image_to_register, homography_matrix, reference.shape[1::-1])
-
+    # Set up registration parameters
+    registration_method = sitk.ImageRegistrationMethod()
+    registration_method.SetMetricAsMeanSquares()  # You can use other metrics as needed
+    registration_method.SetOptimizerAsGradientDescent(learningRate=1.0, numberOfIterations=100, estimateLearningRate=sitk.ImageRegistrationMethod.EachIteration)
+    registration_method.SetInitialTransform(sitk.TranslationTransform(reference_sitk.GetDimension()))
+    
+    # Perform image registration
+    final_transform = registration_method.Execute(reference_sitk, image_to_register_sitk)
+    
+    # Apply the transformation to the image
+    registered_image_sitk = sitk.Resample(image_to_register_sitk, reference_sitk, final_transform, sitk.sitkLinear, 0.0)
+    
+    # Convert the registered image back to a NumPy array
+    registered_image = sitk.GetArrayFromImage(registered_image_sitk)
+    
     return registered_image
+
+
 def stitch_images(registered_images):
     logging.info("Stitching images together...")
     
